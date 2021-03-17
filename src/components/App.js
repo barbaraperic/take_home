b51100465ab7d0adcfe8373b736f4e7093e9e448
Barbara Peric <b.peric10@gmail.com>
2021-03-17 16:30:06 +0100

    gsap animation

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import Loader from "./Loader"
+import React, { useEffect } from 'react';
+//import Loader from "./Loader"
+import { gsap } from "gsap";
 import "./styling.scss"
 
 //process.env.REACT_APP_API_KEY
 
 const App = () => {
+
+
+  useEffect(() => {
+    gsap.to('.ball', {
+      duration: 1,
+      x: 200,
+      scale: 2
+    })
+  }, []);
+
   return (
     <div className="layout">
       <h2>What is the weather outside</h2>
-      <Loader />
+      {/* <img src={sunIcon} alt="sun" id="sunGlobe"/> */}
+      {/* <img src={grade} alt="grade" id="grade"/> */}
+      <div className="ball"></div>
     </div>
   );
 }
---
+++ src/components/animation.js
@@ -0,0 +1,9 @@
+//import { TweenMax, Elastic } from 'gsap';
+import { gsap } from "gsap";
+
+//const ball = document.getElementById("ball");
+gsap.to('.ball', {
+  duration: 1,
+  x: 200,
+  scale: 2
+})
