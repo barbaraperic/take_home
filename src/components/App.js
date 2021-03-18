e5393e08ca73a049db8c2fb9b395999271970a4f
Barbara Peric <b.peric10@gmail.com>
2021-03-18 10:40:22 +0100

    gsap ball rotation

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 //import Loader from "./Loader"
-import { gsap } from "gsap";
+import { TimelineMax, Bounce } from "gsap";
 import "./styling.scss"
 
 //process.env.REACT_APP_API_KEY
@@ -9,19 +9,53 @@ const App = () => {
 
 
   useEffect(() => {
-    gsap.to('.ball', {
-      duration: 1,
-      x: 200,
-      scale: 2
-    })
+
+    let tl = new TimelineMax()
+    
+    tl.staggerFrom(".ball", 1, {
+      y: -300, 
+      ease: Bounce.easeOut
+    }, 0.05)
+
+    tl.add('rotate')
+
+    tl.staggerFrom(".ball", 2, {
+      rotation: 90, 
+      ease: Bounce.easeIn
+    }, 0.05, 'rotate')
+
+    tl.staggerFrom(".ball", 1, {
+      rotation: 180, 
+      ease: Bounce.easeOut
+    }, 0.05, 'rotate += 1.75')
+
+
+
+    tl.from(".newBall", 2, {
+      rotation: 90, 
+      ease: Bounce.easeIn
+    }, 'rotate')
+
+    tl.from(".newBall", 1, {
+      rotation: 180, 
+      ease: Bounce.easeOut
+    }, 'rotate += 1.75')
+
+
   }, []);
 
   return (
-    <div className="layout">
+    <div >
       <h2>What is the weather outside</h2>
       {/* <img src={sunIcon} alt="sun" id="sunGlobe"/> */}
       {/* <img src={grade} alt="grade" id="grade"/> */}
-      <div className="ball"></div>
+      <div className="container">
+        <div className="newBall"></div>
+        <div className="ball"></div>
+        <div className="ball"></div>
+        <div className="ball"></div>
+        <div className="ball"></div>
+      </div>
     </div>
   );
 }
